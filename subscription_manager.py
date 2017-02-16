87cec353f6fcb42e8ecf287ca77b05f1c3cbd4c1
hballard <heath.ballard@gmail.com>
2017-02-15 20:49:08 -0600

    Add requirements file and initial implementation modules

---
+++ subscription_manager.py
@@ -0,0 +1,184 @@
+import redis
+import gevent
+from types import FunctionType
+from promise import Promise
+from graphql import parse, validate, specified_rules, value_from_ast, execute
+from graphql.language.ast import OperationDefinition
+
+
+class RedisPubsub(object):
+
+    def __init__(self, host='localhost', port=6379, *args, **kwargs):
+        self.redis = redis.StrictRedis(host, port, *args, **kwargs)
+        self.pubsub = self.redis.pubsub(ignore_subscribe_messages=True)
+        self.subscriptions = {}
+        self.sub_id_counter = 0
+        self.greenlet = None
+
+    def publish(self, trigger_name, message):
+        self.redis.publish(trigger_name, message)
+        return True
+
+    def subscribe(self, trigger_name, on_message_handler, options):
+        trigger = {}
+        trigger[trigger_name] = on_message_handler
+        self.pubsub.subscribe(**trigger)
+        if not self.greenlet:
+            self.greenlet = gevent.spawn(self.wait_and_get_message)
+        self.sub_id_counter += 1
+        self.subscriptions[self.sub_id_counter] = trigger_name
+        return Promise.resolve(self.sub_id_counter)
+
+    def unsubscribe(self, sub_id):
+        trigger_name = self.subscriptions[sub_id]
+        del self.subscriptions[sub_id]
+        self.pubsub.unsubscribe(trigger_name)
+        if not self.subscriptions:
+            self.greenlet = self.greenlet.kill()
+
+    def wait_and_get_message(self):
+        while True:
+            self.pubsub.get_message()
+            gevent.sleep(.001)
+
+
+class ValidationError(Exception):
+
+    def __init__(self, errors):
+        self.errors = errors
+        self.message = 'Subscription query has validation errors'
+
+
+class SubscriptionManager(object):
+
+    def __init__(self, schema, pubsub, setup_funcs={}):
+        self.schema = schema
+        self.pubsub = pubsub
+        self.setup_funcs = setup_funcs
+        self.subscriptions = {}
+        self.max_subscription_id = 0
+
+    def publish(self, trigger_name, payload):
+        self.pubsub.publish(trigger_name, payload)
+
+    def subscribe(self, query, operation_name, callback, variables, context,
+                  format_error, format_response):
+        parsed_query = parse(query)
+        errors = validate(
+            self.schema,
+            parsed_query,
+            # TODO: Need to create/add subscriptionHasSingleRootField
+            # rule
+            rules=specified_rules
+        )
+
+        if errors:
+            return Promise.reject(ValidationError(errors))
+
+        args = {}
+
+        subscription_name = ''
+
+        for definition in parsed_query.definitions:
+
+            if isinstance(definition, OperationDefinition):
+                root_field = definition.selection_set.selections[0]
+                subscription_name = root_field.name.value
+
+                fields = self.schema.get_subscription_type().get_fields()
+
+                for arg in root_field.arguments:
+
+                    arg_definition = [arg_def for arg_def in
+                                      fields[subscription_name].args if
+                                      arg_def == arg.name.value][0]
+
+                    args[arg_definition.name] = value_from_ast(
+                        arg.value,
+                        arg_definition.type,
+                        variables=variables
+                    )
+
+        if self.setup_funcs[subscription_name]:
+            trigger_map = self.setup_funcs[subscription_name](
+                query,
+                operation_name,
+                callback,
+                variables,
+                context,
+                format_error,
+                format_response,
+                args,
+                subscription_name
+            )
+        else:
+            trigger_map = {}
+            trigger_map[subscription_name] = {}
+
+        external_subscription_id = self.max_subscription_id
+        self.max_subscription_id += 1
+        self.subscriptions[external_subscription_id] = []
+        subscription_promises = []
+
+        for trigger_name in trigger_map.keys():
+            channel_options = trigger_map[trigger_name].get(
+                'channel_options',
+                {}
+            )
+            filter_func = trigger_map[trigger_name].get(
+                'filter_func',
+                True
+            )
+
+            def on_message(root_value):
+                if isinstance(context, FunctionType):
+                    context_promise = Promise(
+                        lambda resolve: resolve(context())
+                    )
+                else:
+                    context_promise = Promise.resolve(context)
+
+                def context_promise_then(context):
+                    if not filter_func(root_value, context):
+                        return
+
+                    execute(
+                        self.schema,
+                        parsed_query,
+                        root_value,
+                        context,
+                        variables,
+                        operation_name
+                    )
+
+                context_promise.then(
+                    context_promise_then
+                ).then(
+                    lambda data: callback(None, data)
+                ).catch(
+                    lambda error: callback(error)
+                )
+
+            subs_promise = Promise(
+                lambda resolve: resolve(self.pubsub.subscribe(
+                    trigger_name,
+                    on_message,
+                    channel_options
+                ))
+            )
+
+            subs_promise.then(
+                lambda id: self.subscriptions[
+                    external_subscription_id].append(id)
+            )
+
+            subscription_promises.append(subs_promise)
+
+        return Promise.all(subscription_promises).then(
+            lambda result: external_subscription_id
+        )
+
+    def unsubscribe(self, sub_id):
+        for internal_id in self.subscriptions.get(sub_id):
+            self.pubsub.unsubscribe(internal_id)
+        self.subscriptions.pop(sub_id, None)
---
+++ subscription_transport_ws.py
@@ -0,0 +1,214 @@
+import json
+from geventwebsocket import WebSocketApplication
+from promise import Promise
+
+SUBSCRIPTION_FAIL = 'subscription_fail'
+SUBSCRIPTION_END = 'subscription_end'
+SUBSCRIPTION_DATA = 'subscription_data'
+SUBSCRIPTION_START = 'subscription_start'
+SUBSCRIPTION_SUCCESS = 'subscription_success'
+KEEPALIVE = 'keepalive'
+INIT = 'init'
+INIT_SUCCESS = 'init_success'
+INIT_FAIL = 'init_fail'
+GRAPHQL_SUBSCRIPTIONS = 'graphql-subscriptions'
+
+# TODO: Implement 'keep_alive' message sent to client that is in
+# apollo subscription-transport constructor
+
+
+class ApolloSubscriptionService(WebSocketApplication):
+
+    def __init__(self, subscription_manager):
+        assert subscription_manager, "Must provide\
+            'subscription_manager' to websocket app constructor"
+        self.subscription_manager = subscription_manager
+        self.connection_subscriptions = {}
+        self.connection_context = {}
+        super(ApolloSubscriptionService, self).__init__()
+
+    def unsubscribe(self, graphql_sub_id):
+        self.subscription_manager.unsubscribe(graphql_sub_id)
+
+    def on_open(self):
+        if self.ws.protocol is None or GRAPHQL_SUBSCRIPTIONS not in self.ws.protocol:
+            self.ws.close(1002)
+
+    def on_close(self):
+        for sub_id in self.connection_subscriptions.keys():
+            self.unsubscribe(self.connection_subscriptions[sub_id])
+            del self.connection_subscriptions[sub_id]
+
+    def on_message(self, message):
+        class nonlocal:
+            on_init_resolve = None
+            on_init_reject = None
+
+        def _init_promise_handler(resolve, reject):
+            nonlocal.on_init_resolve = resolve
+            nonlocal.on_init_reject = reject
+
+        self.connection_context['init_promise'] = Promise(_init_promise_handler)
+
+        try:
+            parsed_message = json.loads(message)
+        except Exception as e:
+            self.send_subscription_fail(
+                None,
+                {'errors': [{'message': str(e)}]}
+            )
+        sub_id = parsed_message.get('id')
+
+        if parsed_message.get('type') == INIT:
+
+            on_connect_promise = Promise.resolve(True)
+            nonlocal.on_init_resolve(on_connect_promise)
+
+            def _init_success_handler(result):
+                if not result:
+                    raise TypeError('Prohibited connection!')
+                return {'type': INIT_SUCCESS}
+
+            self.connection_context['init_promise'].then(
+                _init_success_handler
+            ).catch(
+                lambda error: {
+                    'type': INIT_FAIL,
+                    'error': str(error)}
+            ).then(
+                lambda result: self.send_init_result(result)
+            )
+
+        elif parsed_message.get('type') == SUBSCRIPTION_START:
+
+            def _subscription_start_handler(init_result):
+                base_params = {
+                    'query': parsed_message.get('query'),
+                    'variables': parsed_message.get('variables'),
+                    'operation_name': parsed_message.get('operation_name'),
+                    'context': init_result if isinstance(
+                        init_result, dict) else {},
+                    'format_response': None,
+                    'format_error': None,
+                    'callback': None
+                }
+                promised_params = Promise.resolve(base_params)
+
+                if self.connection_subscriptions[sub_id]:
+                    self.unsubscribe(self.connection_subscriptions[sub_id])
+                    del self.connection_subscriptions[sub_id]
+
+                def _promised_params_handler(params):
+                    if not isinstance(params, dict):
+                        error = 'Invalid params returned from\
+                                OnSubscribe!  Return value must\
+                                be an dict'
+                        self.send_subscription_fail(sub_id, {
+                            'errors': [{'message': error}]
+                        })
+                        raise TypeError(error)
+
+                    def _params_callback(error, result):
+                        if not error:
+                            self.send_subscription_data(sub_id, result)
+                        elif error.errors:
+                            self.send_subscription_data(sub_id, {
+                                'errors': error.errors
+                            })
+                        elif error.message:
+                            self.send_subscription_data(sub_id, {
+                                'errors': [{'message': error.message}]
+                            })
+                        elif error.get('message'):
+                            self.send_subscription_data(sub_id, {
+                                'errors': [{'message': error.get('message')}]
+                            })
+                        else:
+                            self.send_subscription_data(sub_id, {
+                                'errors': [{'message': str(error)}]
+                            })
+
+                    params['callback'] = _params_callback
+
+                    return self.subscription_manager.subscribe(**params)
+
+                def _graphql_sub_id_handler(graphql_sub_id):
+                    self.connection_subscriptions[sub_id] = graphql_sub_id
+                    self.send_subscription_success(sub_id)
+
+                def _error_catch_handler(e):
+                    if e.errors:
+                        self.send_subscription_fail(sub_id, {
+                            'errors': e.errors
+                        })
+                    elif e.message:
+                        self.send_subscription_fail(sub_id, {
+                            'errors': [{'message': e.message}]
+                        })
+                    elif e.get('message'):
+                        self.send_subscription_fail(sub_id, {
+                            'errors': [{'message': e.get('message')}]
+                        })
+                    else:
+                        self.send_subscription_fail(sub_id, {
+                            'errors': [{'message': str(e)}]
+                        })
+
+                promised_params.then(
+                    _promised_params_handler
+                ).then(
+                    _graphql_sub_id_handler
+                ).catch(
+                    _error_catch_handler
+                )
+
+            self.connection_context['init_promise'].then(
+                _init_promise_handler)
+
+        elif parsed_message.get('type') == SUBSCRIPTION_END:
+
+            def _subscription_end_handler(result):
+                if isinstance(self.connection_subscriptions[sub_id], None):
+                    self.unsubscribe(self.connection_subscriptions[sub_id])
+                    del self.connection_subscriptions[sub_id]
+
+            self.connection_context['init_promise'].then(
+                _subscription_end_handler
+            )
+
+        else:
+
+            self.send_subscription_fail(sub_id, {
+                'errors': [{
+                    'message': 'Invalid message type!'
+                }]
+            })
+
+    def send_subscription_data(self, sub_id, payload):
+        message = {
+            'type': SUBSCRIPTION_DATA,
+            'id': sub_id,
+            'payload': payload
+        }
+        self.ws.send(json.dumps(message))
+
+    def send_subscription_fail(self, sub_id, payload):
+        message = {
+            'type': SUBSCRIPTION_FAIL,
+            'id': sub_id,
+            'payload': payload
+        }
+        self.ws.send(json.dumps(message))
+
+    def send_subscription_success(self, sub_id):
+        message = {'type': SUBSCRIPTION_SUCCESS, 'id': sub_id}
+        self.ws.send(json.dumps(message))
+
+    def send_init_result(self, result):
+        self.ws.send(json.dumps(result))  # may need to use promise here
+        if result.get('type') == INIT_FAIL:
+            self.ws.close(1011)
+
+    def send_keep_alive(self):
+        message = {'type': KEEPALIVE}
+        self.ws.send(json.dumps(message))
