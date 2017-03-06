24dd8de4732ce511c9ef918a9948fd9990f9a399
hballard <heath.ballard@gmail.com>
2017-03-06 08:12:41 -0600

    Modified package to include top-level imports in __init__.py

---
+++ graphql_subscriptions/__init__.py
@@ -0,0 +1,4 @@
+from subscription_manager import RedisPubsub, SubscriptionManager
+from subscription_transport_ws import ApolloSubscriptionServer
+
+__all__ = ['RedisPubsub', 'SubscriptionManager', 'ApolloSubscriptionServer']
