e0a40521f154600c86c86de165106cbf3c0c68fa
hballard <heath.ballard@gmail.com>
2017-04-15 22:26:38 -0500

    Add RedisPubsub tests

--- tests/test_subscription_manager.py
+++ tests/test_subscription_manager.py
@@ -1,5 +1,50 @@
-from graphql_subscriptions import subscription_manager
+import pytest
+import redis
+from graphql_subscriptions import RedisPubsub
 
 
-def test_some_test():
-    assert True
+@pytest.fixture(autouse=True)
+def mock_redis(monkeypatch):
+    import fakeredis
+    monkeypatch.setattr(redis, 'StrictRedis', fakeredis.FakeStrictRedis)
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('test', 'test'),
+    ({1: 'test'}, {1: 'test'}),
+    (None, None)
+])
+def test_pubsub_subscribe_and_publish(test_input, expected):
+    ps = RedisPubsub()
+
+    def message_handler(message):
+        assert message == expected
+
+    def publish_callback(sub_id):
+        assert ps.publish('a', test_input)
+
+    ps.subscribe('a', message_handler, {}).then(publish_callback)
+
+    while True:
+        message = ps.pubsub.get_message(ignore_subscribe_messages=True)
+        if message:
+            ps.handle_message(message)
+            break
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('test', 'test'),
+    ({1: 'test'}, {1: 'test'}),
+    (None, None)
+])
+def test_pubsub_subscribe_and_unsubscribe(test_input, expected):
+    ps = RedisPubsub()
+
+    def message_handler(message):
+        assert 0
+
+    def unsubscribe_publish_callback(sub_id):
+        ps.unsubscribe(sub_id)
+        assert ps.publish('a', test_input)
+
+    ps.subscribe('a', message_handler, {}).then(unsubscribe_publish_callback)
